29e974d56678daaf813c147a14a4c6d66b6337c7
kshirley222 <kshirley222@gmail.com>
2020-05-07 10:55:52 -0400

    Co-authored-by: TECopas <thomasecopas@gmail.com>

--- Models/Spell.cs
+++ Models/Spell.cs
@@ -12,6 +12,14 @@ namespace CharacterGenerator.Models
         public string SpellName {get; set;}
         public string SpellSource {get; set;}
         public int SpellLevel {get; set;}
+
+        public Spell(){}
+        public Spell(int SPL_LVL, string source, string name)
+        {
+            SpellLevel = SPL_LVL;
+            SpellSource = source;
+            SpellName = name;
+        }
     }
 
 }
